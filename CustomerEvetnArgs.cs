914282b9a2cb61c1b471e189f8a6bbb16d2c310b
Uncle.Chao <uncle.chao@live.cn>
2014-08-31 17:15:50 +0800

    CustomerEventArgs

---
+++ CustomerEvetnArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartBank
+{
+    public class CustomerEvetnArgs : EventArgs
+    {
+        #region Attributes
+        private string id;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private int balance;
+        public int Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        #endregion
+    }
+}
