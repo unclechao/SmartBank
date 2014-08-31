83f7a6b5ea380ed186bcb92638f73f9bd76562b3
Uncle.Chao <uncle.chao@live.cn>
2014-08-31 17:15:58 +0800

    Main

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer zhangchao = new Customer("220104198905121316", "Zhangchao");
+            zhangchao.Register(zhangchao.Id, zhangchao.Name);
+             zhangchao.Save(zhangchao.Id, 200);
+            zhangchao.Save(zhangchao.Id, 40);
+            zhangchao.closeAccount(zhangchao.Id, zhangchao.Name);
+            Console.ReadKey();
+        }
+    }
+}
